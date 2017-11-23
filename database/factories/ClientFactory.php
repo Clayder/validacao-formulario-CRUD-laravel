cc264a334406f6742854cdb1e7d8c9e5b49883bd
Clayder <peterclayder@gmail.com>
2017-11-22 22:35:59 -0200

    criei o factory do cliente

--- database/factories/ClientFactory.php
+++ database/factories/ClientFactory.php
@@ -4,6 +4,13 @@ use Faker\Generator as Faker;
 
 $factory->define(App\Client::class, function (Faker $faker) {
     return [
-        //
+        'name' => $faker->name,
+        'email' => $faker->email,
+        'phone' => $faker->phoneNumber,
+        'defaulter' => rand(0, 1),
+        'date_birth' => $faker->date(),
+        'sex' => rand(1, 10) % 2 == 0 ? 'm' : 'f',
+        'marital_status' => rand(1, 3),
+        'physical_disability' => rand(1, 10) % 2 == 0 ? $faker->word : null,
     ];
 });
