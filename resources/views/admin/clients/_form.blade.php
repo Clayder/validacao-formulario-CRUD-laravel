9fe6d4fc5a10cd3446dc72a5991a931eccffe75b
Clayder <peterclayder@gmail.com>
2017-12-11 21:05:34 -0200

    recuperei dados do formulário com helper old

--- resources/views/admin/clients/_form.blade.php
+++ resources/views/admin/clients/_form.blade.php
@@ -1,56 +1,63 @@
 {{csrf_field()}}
 <div class="form-group">
     <label for="name">Nome: </label>
-    <input type="text" class="form-control" id="name" name="name" value="{{$client->name}}">
+    <input type="text" class="form-control" id="name" name="name" value="{{old('name',$client->name)}}">
 </div>
 
 <div class="form-group">
     <label for="document_number">Document:</label>
-    <input type="text" class="form-control" id="document_number" name="document_number" value="{{$client->document_number}}">
+    <input type="text" class="form-control" id="document_number" name="document_number" value="{{old('document_number',$client->document_number)}}">
 </div>
 
 <div class="form-group">
     <label for="email">E-mail: </label>
-    <input type="email" class="form-control" id="email" name="email" value="{{$client->email}}">
+    <input type="email" class="form-control" id="email" name="email" value="{{old('email',$client->email)}}">
 </div>
 
 <div class="form-group">
     <label for="phone"> Telefone: </label>
-    <input type="text" class="form-control" id="phone" name="phone" value="{{$client->phone}}">
+    <input type="text" class="form-control" id="phone" name="phone" value="{{old('phone',$client->phone)}}">
 </div>
+
+@php
+    $marital_status = $client->marital_status;
+@endphp
 
 <div class="form-group">
     <label for="marital_status">Estado civil </label>
-    <select name="marital_status" id="marital_status" class="form-control" value="{{$client->marital_status}}">
+    <select name="marital_status" id="marital_status" class="form-control" value="{{$marital_status}}">
         <option value="">Selecione o estado civil</option>
-        <option value="1" {{$client->marital_status == 1 ? 'selected="selected"' : ''}}>Solteiro</option>
-        <option value="2" {{$client->marital_status == 2 ? 'selected="selected"' : ''}}>Casado</option>
-        <option value="3" {{$client->marital_status == 3 ? 'selected="selected"' : ''}}>Divorciado</option>
+        <option value="1" {{old('marital_status', $marital_status) == 1 ? 'selected="selected"' : ''}}>Solteiro</option>
+        <option value="2" {{old('marital_status', $marital_status) == 2 ? 'selected="selected"' : ''}}>Casado</option>
+        <option value="3" {{old('marital_status', $marital_status) == 3 ? 'selected="selected"' : ''}}>Divorciado</option>
     </select>
 </div>
 
 <div class="form-group">
     <label for="date_birth">Data Nasc</label>
-    <input type="date" class="form-control" name="date_birth" id="date_birth" value="{{$client->date_birth}}">
+    <input type="date" class="form-control" name="date_birth" id="date_birth" value="{{old('date_birth',$client->date_birth)}}">
 </div>
 
+@php
+    $sex = $client->sex;
+@endphp
 <div class="radio">
     <label>
-        <input type="radio" name="sex" value="m" {{$client->sex == 'm' ? 'checked = "checked"':''}}> Masculino
+        <input type="radio" name="sex" value="m" {{old('sex',$sex == 'm' ? 'checked = "checked"':'')}}> Masculino
     </label>
 </div>
 <div class="radio">
     <label>
-        <input type="radio" name="sex" value="f" {{$client->sex == 'f' ? 'checked = "checked"':''}}> Feminino
+        <input type="radio" name="sex" value="f" {{old('sex',$sex == 'f' ? 'checked = "checked"':'')}}> Feminino
     </label>
 </div>
 <div class="form-group">
     <label for="physical_disability">Deficiência Física</label>
-    <input type="text" class="form-control" name="physical_disability" id="physical_disability" value="{{$client->physical_disability}}">
+    <input type="text" class="form-control" name="physical_disability" id="physical_disability" value="{{old('physical_disability',$client->physical_disability)}}">
 </div>
 <div class="checkbox">
     <label>
-        <input type="checkbox" class="" name="defaulter" id="defaulter" {{$client->defaulter ? 'checked = "checked"':''}}> Inadimplente ?
+        <input type="checkbox" class="" name="defaulter" id="defaulter" {{old('defaulter',$client->defaulter) ? 'checked = "checked"':''}}> Inadimplente ?
     </label>
 </div>
 
